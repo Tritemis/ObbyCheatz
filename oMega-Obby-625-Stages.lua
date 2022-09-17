b6db35d9438aacf1995170739e3699fafb2af5a5
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-09-17 14:37:16 +0200

    https://www.roblox.com/games/8003084678/

---
+++ oMega-Obby-625-Stages.lua
@@ -0,0 +1,5 @@
+while true do
+    wait(0.05)
+    local NextStage = game.Players.LocalPlayer.leaderstats.Stage.value + 1;
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Stages[NextStage].Spawn.CFrame;
+end
