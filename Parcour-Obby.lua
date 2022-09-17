37d4a4bcbb3bbcd534e0806c69b914bca062cf25
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-09-17 14:58:31 +0200

    https://www.roblox.com/games/9295827626/

--- Parcour-Obby.lua
+++ Parcour-Obby.lua
@@ -1,5 +1,5 @@
 while true do
-    wait(0.5)
+    wait(0.05)
     local NextStage = game.Players.LocalPlayer.leaderstats.Stage.Value + 1;
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.Stages[NextStage].CFrame;
 end
