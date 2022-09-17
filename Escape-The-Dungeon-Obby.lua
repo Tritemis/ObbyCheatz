9f818ed7a3d6be41756f2ae2fdf66f6ae343b40d
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-09-17 15:44:30 +0200

    https://www.roblox.com/games/2028593804/

---
+++ Escape-The-Dungeon-Obby.lua
@@ -0,0 +1,43 @@
+wait(0.05);
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 1"].CFrame;
+wait(0.05);
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 2"].CFrame;
+wait(0.05);
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 3"].CFrame;
+wait(0.05);
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 4"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 5"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 6"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 7"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 8"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 9"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 10"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 11"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 12"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 13"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 14"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 15"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 16"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 17"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 18"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 19"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["Checkpoint 20"].CFrame;
+wait(0.05)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").WinnersBadge.Platform.CFrame;
+print("Worked!")
